41a5afd44127c6d31fb15397b1dc83cc1a463713
Yogendra Singh <yogendra.singh@i2econsulting.com>
2022-09-26 22:03:27 +0530

    first commit

---
+++ usersApiData.js
@@ -0,0 +1,85 @@
+const data=[
+    {
+        id:'101',
+        firstname:'John',
+        lastname:'cena',
+        email:'john@gmail.com',
+        mobile:'9152368740',
+        address:'3Land O Lakes Florida (FL) US'
+    },
+    {
+        id:'102',
+        firstname:'Mark',
+        lastname:'Zukerberg',
+        email:'mark@gmail.com',
+        mobile:'9012368740',
+        address:'360 Sunnyside Ln, Tahoe City, CA 96145.'
+    },
+    {
+        id:'103',
+        firstname:'Sundar',
+        lastname:'pichai',
+        email:'sundar@gmail.com',
+        mobile:'+91523600225',
+        address:' 1600 Amphitheatre Parkway, Mountain View, CA 94043'
+    },
+    {
+        id:'104',
+        firstname:'Bill',
+        lastname:'gates',
+        email:'bill@gmail.com',
+        mobile:'+9100368740',
+        address:'2/208 huston road washignton Dc'
+    },
+    {
+        id:'105',
+        firstname:'Rock',
+        lastname:'teja',
+        email:'rock@gmail.com',
+        mobile:'8152368740',
+        address:'500/4 gruton uk'
+    },
+    {
+        id:'106',
+        firstname:'Tejal',
+        lastname:'shinde',
+        email:'tejal@gmail.com',
+        mobile:'862368740',
+        address:'58/55 bruno road mumbai'
+    },
+    {
+        id:'107',
+        firstname:'Satya',
+        lastname:'nadela',
+        email:'satya@gmail.com',
+        mobile:'866368740',
+        address:'44/2 ampta road near milk dairy uk'
+    },
+    {
+        id:'108',
+        firstname:'Yogendra',
+        lastname:'Yadav',
+        email:'yogendra@gmail.com',
+        mobile:'8126279856',
+        address:'Yadav market dhanipur mandi aligarh'
+    },
+    {
+        id:'109',
+        firstname:'Sandy',
+        lastname:'Goos',
+        email:'sandy@gmail.com',
+        mobile:'+520368740',
+        address:'00/23 snady nivas texas US'
+    },
+    {
+        id:'110',
+        firstname:'Vijay',
+        lastname:'Chpra',
+        email:'vijay@gmail.com',
+        mobile:'865972014',
+        address:'7/11 city road andheri mumbai'
+    }
+
+]
+
+module.exports=data;
